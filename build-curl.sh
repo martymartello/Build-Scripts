martymartello/Build-Scripts

commit 80bbe1b013561f85b604d60c5d92f12d897d1c0d

Bump cURL version

build-curl.sh
@@ -3,7 +3,7 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds cURL from sources.
 
-CURL_VER=7.86.0
+CURL_VER=7.88.1"
 CURL_TAR="curl-${CURL_VER}.tar.gz"
 CURL_DIR="curl-${CURL_VER}"
 PKG_NAME=curl

commit 92b7a836f4f0c86667d6b21dcdd0f63373ee26fb

Removed a typo on build-curl.sh introduced with commit 80bbe1b that prevents execution

build-curl.sh
@@ -3,7 +3,7 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds cURL from sources.
 
-CURL_VER=8.4.0"
+CURL_VER=8.4.0
 CURL_TAR="curl-${CURL_VER}.tar.gz"
 CURL_DIR="curl-${CURL_VER}"
 PKG_NAME=curl